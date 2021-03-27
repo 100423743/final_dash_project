d3f44a366efc6d25ce1cbc3031b93d0b942b9465
mariacristinasi <100425253@alumnos.uc3m.es>
2021-03-27 14:45:32 +0100

    Creating first tabs

--- app.py
+++ app.py
@@ -4,5 +4,22 @@ import dash_core_components as dcc
 
 import pandas as pd
 
+
+app = dash.Dash(__name__, title="Dash Project - Ignacio Medina & María Cristina Sánchez")
+
 df_url_rocks = 'https://query.data.world/s/xzozlqhuagxyazzgc3avtgcaw2yqxk'
 df_rocks = pd.read_csv(df_url_rocks)
+
+app.layout= html.Div([
+    html.Div([html.H1(app.title, className="app-header--title")],
+        className= "app-header",
+    ),
+    html.Div([  
+        dcc.Tabs(id="tabs", value='tab-fifa', children=[
+            dcc.Tab(label='FIFA', value='tab-fifa'),
+            dcc.Tab(label='Rocks', value='tab-rocks'),
+        ]),
+        html.Div(id='tabs-content')
+    ],
+    className= "app-body")
+])
