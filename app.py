c01f0eed6a659f5790c85c275ded43ab395133a0
mariacristinasi <100425253@alumnos.uc3m.es>
2021-03-27 14:28:05 +0100

    Reading file

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import dash
 import dash_html_components as html
 
 import pandas as pd
+
+df_url_rocks = 'https://query.data.world/s/xzozlqhuagxyazzgc3avtgcaw2yqxk'
+df_rocks = pd.read_csv(df_url_rocks)
